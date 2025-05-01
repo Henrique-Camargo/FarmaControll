316373e939a09916572ad2f3e205c17ebd84f30d
Henrique-Camargo <henriquecnobrega0711@gmail.com>
2025-05-01 20:06:33 -0300

    DataBase Working, H2

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import model.Medicament;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicamentRepository extends JpaRepository<Medicament, Long> {
+}
